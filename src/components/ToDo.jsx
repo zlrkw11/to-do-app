d347c348d74a0963fd8ec9f2cbdd80e9923d9699
ZL <ray040406@gmail.com>
2024-06-28 00:48:14 +1200

    state hook for todo list

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 export default function ToDo() {
   const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
   function handleSubmit(e) {
     // prevent refreshing
     e.preventDefault();
   }
+
   return (
     <div>
       <form onSubmit={handleSubmit}>
@@ -13,7 +15,9 @@ export default function ToDo() {
           value={todo}
           onChange={(e) => setTodo(e.target.value)}
         />
-        <button type="submit">Add</button>
+        <button type="submit" onClick={}>
+          Add
+        </button>
       </form>
     </div>
   );
