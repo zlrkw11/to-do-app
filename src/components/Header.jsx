9f5d7a123ede39fa87a2301ad179f9c9d443704d
ZL <ray040406@gmail.com>
2024-06-30 00:29:04 +1200

    styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import styles from "./header.module.css";
 function Header() {
-  return <div className={styles.header}></div>;
+  return <div className={styles.header}>x</div>;
 }
 
 export default Header;
