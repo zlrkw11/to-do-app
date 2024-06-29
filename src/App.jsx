4eec6c6b11c7262f1d33ec33977f9154de11b438
ZL <ray040406@gmail.com>
2024-06-30 00:30:50 +1200

    styled app.jsx itself

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import "./App.css";
 import ToDo from "./components/ToDo";
 import Header from "./components/Header";
+
 function App() {
   return (
-    <>
+    <div className="App">
       <Header />
       <ToDo />
-    </>
+    </div>
   );
 }
 
