ce719d0a77f8b1a512a7e2beb29ab7a1d8192c8b
ZL <ray040406@gmail.com>
2024-06-30 17:23:51 +1200

    info

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -5,7 +5,9 @@ export default function Todo() {
   const [todos, setTodos] = useState([]);
   return (
     <div>
+      {/* Form component runs and provides a todo list */}
       <Form todos={todos} setTodos={setTodos} />
+      {/* TodoList component renders the list */}
       <TodoList todos={todos} />
     </div>
   );
