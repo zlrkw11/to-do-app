cd89fe90bb00b22810e2e6353a776fafdc12064b
ZL <ray040406@gmail.com>
2024-07-02 19:02:36 +1200

    passing item id to delete handle function

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,11 +1,19 @@
 import styles from "./todolist.module.css";
 function TodoItem({ item }) {
+  function handleDelete(item) {
+    console.log("Delete button clicked for item", item);
+  }
   return (
     <div className={styles.item}>
       <div className={styles.itemName}>
         {item}
         <span>
-          <button className={styles.deleteButton}>x</button>
+          <button
+            onClick={() => handleDelete(item)}
+            className={styles.deleteButton}
+          >
+            x
+          </button>
         </span>
       </div>
 
